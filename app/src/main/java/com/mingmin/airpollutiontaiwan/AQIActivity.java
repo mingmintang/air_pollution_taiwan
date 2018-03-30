f5ac26cebf4272c3182ef2a815b3681af021051b
Mingmin Tang <mingmintang0331@gmail.com>
2018-03-30 21:52:03 +0800

    Initial commit

---
+++ app/src/main/java/com/mingmin/airpollutiontaiwan/AQIActivity.java
@@ -0,0 +1,13 @@
+package com.mingmin.airpollutiontaiwan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AQIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aqi);
+    }
+}
